88e08a588f1ec4434ff591711ba4702357768cfe
reza_mzv99 <reza_mzv99@proton.me>
2026-05-27 00:27:14 +0330

    implement generic views (list, detail, create, update, and delete).

--- books/models.py
+++ books/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    price = models.DecimalField(decimal_places=2, max_digits=10)
     category = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse ('book_detail', kwargs={'pk': self.pk})
--- books/views.py
+++ books/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+from books.models import Book
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'books/books_list.html'
+    context_object_name = 'book_list'
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    template_name = 'books/book_create.html'
+    fields = '__all__'
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    template_name = 'books/book_update.html'
+    fields = '__all__'
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = 'books/book_delete.html'
+    success_url = reverse_lazy('book_list')
