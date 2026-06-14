6f914945334d59c1f09d32e2dbc201e5ab46150a
reza_mzv99 <reza_mzv99@proton.me>
2026-06-14 20:03:07 +0330

    add new book categories in html and depdown in models.py

--- books/models.py
+++ books/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from django.urls import reverse
 
 class Book(models.Model):
+    CATEGORY_CHOICES = [
+        ('fiction', 'Fiction'),
+        ('nonfiction', 'Non-Fiction'),
+        ('fantasy', 'Fantasy'),
+        ('classics', 'Classics'),
+        ('education', 'Education'),
+        ('children', 'Children'),
+        ('new_arrivals', 'New Arrivals'),
+    ]
+
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
     price = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
