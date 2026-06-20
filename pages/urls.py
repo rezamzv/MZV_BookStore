b89dc1cd7a69e961fdcf7d6aec5d12627863db82
reza_mzv99 <reza_mzv99@proton.me>
2026-06-20 13:37:47 +0330

    implement search feature.

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from pages.views import HomePageView, CategoryListView
+from pages.views import HomePageView, CategoryListView, search_list
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home_page'),
+    path('search/', search_list, name='search_list'),
     path('<str:category>/', CategoryListView.as_view(), name='category_list'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.template.context_processors import request
 from django.views import generic
+from django.db.models import Q
 
 from books.forms import BookForm
 from books.models import Book
@@ -22,6 +24,21 @@ class CategoryListView(generic.ListView):
     def get_queryset(self):
         category = self.kwargs.get('category')
         return Book.objects.filter(category=category)
+
+
+def search_list(request):
+    query = request.GET.get('q','')
+    books = Book.objects.all()
+    if query:
+        books = books.filter(
+            Q(title__icontains=query) | Q(author__icontains=query) | Q(category__icontains=query)
+        )
+    return render(request, 'pages/search_list.html', {
+        'books': books,
+        'query': query,
+    })
+
+
 
 
 
