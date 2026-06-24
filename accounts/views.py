1e2c4341a67f3d3b9236cad0ee7abb91e099ad0f
reza_mzv99 <reza_mzv99@proton.me>
2026-06-24 18:15:29 +0330

    implement reset password and pasword change into templates.(send email still have bugs)

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+
 from accounts.forms import CustomUserCreationForm
 
 
 # Create your views here.
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    template_name = 'registration/signup.html '
+    template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
